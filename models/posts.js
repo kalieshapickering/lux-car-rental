f131b20567b3449139b961cc9696d52c33aeb19b
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-10-07 00:08:41 -0400

    Configured Sequelize ORM for whole directory

--- models/posts.js
+++ models/posts.js
@@ -1,6 +1,41 @@
-const path = require("path");
-
-// load sequelize connection
-const Sequelize = require("sequelize");
-const sequelize = require(path.join(__dirname, "..", "config", "connection"));
-
+module.exports = function (sequelize, DataTypes) {
+    const Posts = sequelize.define("posts", {
+        req_loc: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "error retrieving IP"
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            defaultValue: ""
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        vehicle: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        start_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        end_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    }, {
+        underscored: true,
+        freezeTableName: true
+    });
+    return Posts;
+};
